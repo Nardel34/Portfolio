23d41d32a1161996c2c8729d5b92fb0595c34279
Victor Nardella <victor.nardel@gmail.com>
2022-09-14 22:43:45 +0200

    Actualisation info v2

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="google-site-verification" content="SyvyM9KBu-vKRwnBnZOYICzgYBLZu5AVFouIYj9X2ww" />
 	<title>Victor Nardella - Portfolio</title>
 	<?php include 'assets/php/head.php' ?>
-	<meta name="description" content="Victor Nardella - Développeur Fullstack à la recherche d'alternance. Je développe des sites web et logiciels à partir du cahier des charges jusqu'à la mise en ligne ou en service.">
+	<meta name="description" content="Victor Nardella - Développeur Fullstack. Je développe des sites web et logiciels à partir du cahier des charges jusqu'à la mise en ligne ou en service.">
 	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
 </head>
 
@@ -23,22 +23,19 @@
 			<div class="a_propos">
 				<h1>À propos de moi</h1>
 				<p>
-					Présentement étudiant de seconde année en BTS SIO (Services Informatiques aux
-					Organisations) au Lycée CCI Gard à Nîmes (30), en spécialité SLAM (Solutions Logicielles et Applications
-					Métiers), j'aspire à intégrer une entreprise en tant qu'alternant afin de découvrir le milieu
-					professionnel du domaine de l'informatique, le tout en continuant mes études.
+					Présentement étudiant de troisième année en Licence informatique à l'EPSI Montpellier (34)
+					en spécialité Data Scientist et en alternance au sain de l'entreprise Queoval basé à Mauguio.
 				</p>
 				<br>
 				<p>
-					Le choix de la spécialité SLAM était pour moi, une évidence tant je suis
-					un passionné d'informatique et de nouvelles technologies mais plus particulièrement du domaine
-					de la programmation. Ce qui m'amène à mettre à profit beaucoup de mon temps libre à en apprendre d'avantage
-					sur différents langages de programmation.
+					La voie du développement informatique était pour moi, une évidence tant je suis
+					un passionné d'informatique et de nouvelles technologies. Ce qui m'amène à mettre à
+					profit beaucoup de mon temps libre à en apprendre d'avantage sur différents langages de programmation.
 				</p>
 				<br>
 				<p>
 					Vous pourrez retrouver dans mon portfolio, toutes les compétences que j'ai acquises au cours de mes
-					années d'études ainsi qu'en auto-formation.
+					années d'études ainsi qu'en auto-formation et dans le milieu professionnel.
 				</p>
 			</div>
 			<div class="line">
@@ -52,18 +49,19 @@
 				<p>2016 : Brevet des collèges DNB</p>
 				<br><br>
 				<h2>Expériences professionnelles :</h2><br>
-				<p>2020-21 : 5 mois en tant qu'employé polyvalent au Centre de tri du Pays de l’Or</p><br>
+				<p>2022 : Développeur / intégrateur web, ASP.NET et Angular CLI (En cours)</p><br>
+				<p>2022 : 1 mois en tant que développeur PHP au sain du Cabinet Origami</p><br>
+				<p>2020-22 : 8 mois en tant qu'employé polyvalent au Centre de tri du Pays de l’Or</p><br>
 				<p>2019 : 2 mois en tant qu'employé polyvalent de restauration au Burger King de Saint-Aunès</p><br>
-				<p>2019 : 1 mois en tant qu'employé polyvalent au service technique de la Mairie de Saint-Aunès</p><br>
-				<p>2016 : 1 semaine de stage d'observation à la cave Les Coteaux de Saint-Christol</p>
+				<p>2019 : 1 mois en tant qu'employé polyvalent au service technique de la Mairie de Saint-Aunès</p>
 			</div>
 			<div class="line">
 			</div>
 			<div class="projet">
-				<h1>Projets après le BTS</h1>
+				<h1>Projets après la Licence</h1>
 				<p>
-					À la suite de mon BTS SIO, j'ai pour projet de poursuivre mes études avec l'obtention d'un Bachelor informatique à
-					l'EPSI Montpellier puis de conclure avec un master option Cybersécurité.
+					À la suite de ma Licence, j'ai pour projet de poursuivre mes études avec l'obtention d'un Master option Cybersécurité à
+					l'EPSI Montpellier.
 				</p>
 			</div>
 		</section>
